843ef3894e7296b9e7674b863d674dee7283e1ea
CauaTavares <cauaxd2010@outlook.com>
2022-09-25 15:33:50 -0300

    Correção

--- cursojs/aula14ex/ex016/script.js
+++ cursojs/aula14ex/ex016/script.js
@@ -6,7 +6,6 @@ function contar() {
 
     /*ESSE SINAL (||) SIGNIFICA (OU). OTARIO!! (17/09/2022-11:30) TAVARESCAUA
             SE O (INICIO){ini} TIVER VAZIO OU SE O (FIM){fim} TIVER VAZIO OU O (PASSO){passo} TIVER VAZIO VAI DAR ALERT
-        PRESTENÇÃO OTARIO
     significado do codigo*/
 
     if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
